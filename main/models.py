72daec85a97a72330866c9450aa14a0597dfff68
asShidqi <fayyed76@gmail.com>
2024-09-18 08:28:20 +0700

    add tugas 2

--- main/models.py
+++ main/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
-class MoodEntry(models.Model):
+import uuid
+class CatEntry(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
--- main/views.py
+++ main/views.py
@@ -1,11 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from main.forms import CatEntryForm
+from main.models import CatEntry
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def show_main(request):
+    cat_entries = CatEntry.objects.all()
     context = {
         'name_app' : 'Cat Shop',
         'name': 'Muhammad Fayyed As Shidqi',
-        'class': 'PBP D'
+        'class': 'PBP D',
+        'cat_entries': cat_entries
     }
 
     return render(request, "main.html", context)
+
+def create_cat_entry(request):
+    form = CatEntryForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+    
+    context = {'form':form}
+    return render(request,"create_cat_entry.html",context)
+
+def show_xml(request):
+    data = CatEntry.objects.all()
+
+def show_xml(request):
+    data = CatEntry.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = CatEntry.objects.all()
+
+def show_json(request):
+    data = CatEntry.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = CatEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = CatEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
